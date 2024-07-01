cbe375d6985806082f41b954d4c56f0eade98b74
Calcod3 <calnjoroge@gmail.com>
2024-07-02 01:16:02 +0300

    initial

---
+++ lib/pages/home/widgets/activity_details_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard/Responsive.dart';
+import 'package:flutter_dashboard/model/health_model.dart';
+import 'package:flutter_dashboard/widgets/custom_card.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ActivityDetailsCard extends StatelessWidget {
+  const ActivityDetailsCard({super.key});
+
+  final List<HealthModel> healthDetails = const [
+    HealthModel(
+        icon: 'assets/svg/asg.svg', value: "Atlantic Summer Games", title: "2024"),
+    HealthModel(icon: 'assets/svg/braga.svg', value: "Braga Games", title: "2024"),
+    HealthModel(
+        icon: 'assets/svg/promofit.svg', value: "PromoFit Games - XVIII Edition", title: "25-26 May 2024"),
+    HealthModel(icon: 'assets/svg/more.svg', value: "More Events", title: "2024"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: healthDetails.length,
+      shrinkWrap: true,
+      physics: const ScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: Responsive.isMobile(context) ? 2 : 4,
+          crossAxisSpacing: !Responsive.isMobile(context) ? 15 : 12,
+          mainAxisSpacing: 12.0),
+      itemBuilder: (context, i) {
+        return CustomCard(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                healthDetails[i].icon,
+                width: 90,
+                height: 90,
+                ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 4),
+                child: Text(
+                  healthDetails[i].value,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Text(
+                healthDetails[i].title,
+                style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.normal),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
